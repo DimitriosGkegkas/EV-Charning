fbfb8a6880423c94308a934789b7915fad4d8de9
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2020-12-27 11:39:19 +0200

    Add backend

---
+++ backend/src/test/java/team39/evcharging/ntua/gr/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package team39.evcharging.ntua.gr.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
