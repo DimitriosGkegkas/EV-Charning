047f11a2b9b1ae17d9f8482e6c36d76ab07cff20
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2021-02-27 11:15:31 +0200

    Changed to NODE

--- backend/src/main/java/team39/evcharging/ntua/gr/backend/models/UserRepository.java
+++
@@ -1,5 +0,0 @@
-package team39.evcharging.ntua.gr.backend.models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {}
