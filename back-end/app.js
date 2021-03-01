fd1d4784a384766c68b202b7195546ed9a5e3e84
andrianaavgerinou <andriana.avg@gmail.com>
2021-03-01 21:10:10 +0200

    Add login, logout, create user, find by username

--- back-end/app.js
+++ back-end/app.js
@@ -3,13 +3,20 @@ const databaseURL = require('./database/auth');
 const express = require('express');
 const bodyParser= require('body-parser');
 const app = express();
+const userRoute = require("./routes/userRoutes");
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(userRoute );
+
+
 
 
 mongoose.connect(databaseURL.URL, { useNewUrlParser: true , useUnifiedTopology: true })
     .then(
         result => {
-            app.listen(8080);
+            app.listen(8765);
         }
     )
     .catch(
@@ -17,3 +24,5 @@ mongoose.connect(databaseURL.URL, { useNewUrlParser: true , useUnifiedTopology: 
             console.log(err);
         }
     );
+
+
