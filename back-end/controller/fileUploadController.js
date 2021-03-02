e7229f0191f9d9cc0d15029bce5472bd4f680473
AnnaXar <annaxar3@gmail.com>
2021-03-02 13:23:34 +0200

    Fix file upload controller function

--- back-end/controller/fileUploadController.js
+++ back-end/controller/fileUploadController.js
@@ -1,4 +1,7 @@
 const multer = require("multer");
+const csv = require('csv-parser');
+const fs = require('fs');
+const Session = require('./../model/session');
 
 exports.csvFilter = (req, file, cb) => {
   if (file.mimetype.includes("csv")) {
@@ -13,12 +16,10 @@ exports.storage = multer.diskStorage({
     cb(null, __dirname + "/../resources/uploads/");
   },
   filename: (req, file, cb) => {
-    console.log(file.originalname);
     cb(null, `${Date.now()}-bezkoder-${file.originalname}`);
   },
 });
 
-var csv = require('fast-csv');
 var mongoose = require('mongoose');
 var session= require('./../model/session');
 
@@ -47,3 +48,50 @@ exports.upload = function (req, res) {
 		 res.send(authors.length + ' authors have been successfully uploaded.');
 	 });
 };
+
+
+exports.uploadToDB= (req, res) => {
+	const filePath = req.file.path;
+	let InUploadedFile = 0;
+	let Imported = 0
+	fs.createReadStream(filePath).pipe(csv()).on('data', (row) => {
+		InUploadedFile = InUploadedFile + 1;
+		const session = new Session({
+			sessionID: row["sessionID"],
+			connectionTime: row["connectionTime"],
+			disconnectTime: row["disconnectTime"],
+			doneChargingTime: row["doneChargingTime"],
+			kWhDelivered: row["kWhDelivered"]
+		})
+
+		session.save().then(() => { Imported = Imported + 1; })
+			.catch(err => {
+				if (err.code === 11000) {
+
+				}
+				else {
+					console.log(err)
+				}
+			})
+
+	})
+		.on('end', () => {
+			Session.find().exec(function (err, results) {
+				var SeassionsInDatabase = results.length
+				res.status(200).json({
+					SeassionsInUploadedFile: InUploadedFile,
+					SessionsImported: Imported,
+					TotalSeassionsInDatabase: SeassionsInDatabase
+				})
+			});
+			fs.unlink(filePath, (err) => {
+				if (err) {
+					console.error(err)
+					return
+				}
+			}
+			)
+		})
+
+
+}
