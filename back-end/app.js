c4a903b974b7a957e72652b1e3cd9cb87871659e
AnnaXar <annaxar3@gmail.com>
2021-03-02 18:47:29 +0200

    Add https

--- back-end/app.js
+++ back-end/app.js
@@ -6,15 +6,19 @@ const app = express();
 const userRoute = require("./routes/userRoutes");
 const fileRoute = require("./routes/fileUploadRoutes");
 const helpRoute = require("./routes/helpRoutes");
-
+const https = require('https')
+const fs = require('fs')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(userRoute );
 app.use(helpRoute );
 app.use(fileRoute);
 
+https.createServer({
+    key: fs.readFileSync('database/server.key'),
+    cert: fs.readFileSync('database/server.cert')
+  }, app).listen(8765)
 
-app.listen(8765);
 
 mongoose.connect(databaseURL.URL, { useNewUrlParser: true , useUnifiedTopology: true })
     .catch(
