fc41ed71bdee7cea66557bed80ef124336e69e77
andrianaavgerinou <andriana.avg@gmail.com>
2021-03-05 21:34:57 +0200

    Add error handling

--- back-end/controller/userController.js
+++ back-end/controller/userController.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const secretKey = require('../database/secretKey');
 const redis = require('redis');
-const JWTR =  require('jwt-redis').default;
+const JWTR = require('jwt-redis').default;
 const redisClient = redis.createClient();
 const jwtr = new JWTR(redisClient);
 
@@ -55,66 +55,79 @@ exports.signup = (req, res, next) => {
                     }
                 )
         })
-        .catch(err =>{
-            res.status(400).json({message:"Bad Arguments"})
-        }
-            )
-}
-
-exports.getuser = (req, res, next) => {
-
-    const username = req.params.username;
-    userSchema.findOne({ 'username': username }, (err, result) => { return result })
-        .then(result => {
-            res.status(200).json(
-                result
-            )
+        .catch(err => {
+            res.status(400).json({ message: "Bad Arguments" })
         }
         )
 }
 
+exports.getuser = (req, res, next) => {
+    const username = req.params.username; 
+    userSchema.findOne({ 'username': username }, (err, result) => {   
+        return result
+    })
+        .then(result => {
+            if (result===null) {
+                res.status(400).json({
+                    message: "Please insert a correct username"
+                })
+            }
+            else {
+            res.status(200).json(
+                result
+            )
+            }
+        })
+        .catch(err => {
+            res.status(err.statusCode).json({
+                message: err.message
+            })
+            return
+        })
+}
 
-exports.login= (req, res, next) => {
+
+exports.login = (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
     let loadUser;
     userSchema.findOne({ 'username': username }, (err, result) => { return result })
-    .then(user => {
-        if(!user){
-            const error = new Error("A user with this username could not be found");
-            error.statusCode = 400;
-            throw error;
-        }
-        loadUser = user;
-        return bcrypt.compare(password, user.password);
-    })
-    .then( isEqual =>{
-        if(!isEqual){
-            const error = new Error("Wrong Password");
-            error.statusCode = 401;
-            throw error;
-        }
-        jwtr.sign({
-            userId: loadUser._id.toString()
-        },
-        secretKey.key,
-        {expiresIn:'1h'}
-        ).then(token=>res.status(200).json({
-            token: token
-        }))
+        .then(user => {
+            if (!user) {
+                const error = new Error("A user with this username could not be found");
+                error.statusCode = 400;
+                throw error;
+            }
+            loadUser = user;
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+            if (!isEqual) {
+                const error = new Error("Wrong Password");
+                error.statusCode = 401;
+                throw error;
+            }
+            jwtr.sign({
+                userId: loadUser._id.toString()
+            },
+                secretKey.key,
+                { expiresIn: '1h' }
+            ).then(token => res.status(200).json({
+                token: token
+            }))
 
-    })
-    .catch(err =>
-        {
+        })
+        .catch(err => {
             res.status(401).json({
                 message: "Not authorized: Please insert correct password"
             })
-            next(err)}
+            next(err)
+        }
         )
-    
+
 }
 
-exports.logout= (req, res, next) => {
+exports.logout = (req, res, next) => {
     const token = req.header('Authorization').split(' ')[1];
     jwtr.destroy(token).then(res.status(200).json({}))
 
--- cli-client/src/admin.js
+++ cli-client/src/admin.js
@@ -1,31 +1,33 @@
 const request = require('request');
 const https = require('https');
-const fs =require('fs');
+const fs = require('fs');
 
 exports.Admin = (username, password) => {
-    const jsonObject = 
-        {"username": username,
-        "password": password}
+    const jsonObject =
+    {
+        "username": username,
+        "password": password
+    }
     const agentOptions = {
         host: 'localhost'
         , port: '8765'
         , path: '/admin/usermod'
- 
+
         , rejectUnauthorized: false
     };
     const agent = new https.Agent(agentOptions);
-    const auth= "Bearer "+fs.readFileSync('softeng20bAPI.token');
+    const auth = "Bearer " + fs.readFileSync('softeng20bAPI.token');
     request({
         url: "https://localhost:8765/admin/usermod"
         , method: 'POST'
-        ,headers : {
-            "Authorization" : auth
+        , headers: {
+            "Authorization": auth
         }
-        , json: jsonObject 
+        , json: jsonObject
         , agent: agent
     }, function (err, resp, body) {
-        console.log( body)
-        
+        console.log(body)
+
 
     });
 }
@@ -34,21 +36,41 @@ exports.findUser = (username) => {
     const agentOptions = {
         host: 'localhost'
         , port: '8765'
-        , path: '/admin/users/'+username
- 
+        , path: '/admin/users/' + username
+
         , rejectUnauthorized: false
     };
     const agent = new https.Agent(agentOptions);
-    const auth= "Bearer "+fs.readFileSync('softeng20bAPI.token');
+    let token
+    try {
+        token = fs.readFileSync('softeng20bAPI.token');
+    }
+    catch {
+        console.log("Access Denied")
+        return
+    }
+    const auth = "Bearer " + token;
+
     request({
-        url: "https://localhost:8765/admin/users/"+username
+        url: "https://localhost:8765/admin/users/" + username
         , method: 'GET'
-        ,headers : {
-            "Authorization" : auth
+        , headers: {
+            "Authorization": auth
         }
         , agent: agent
     }, function (err, resp, body) {
-        console.log( body)
-        
+        if (err) {
+            console.log(err.message)
+        }
+        else if (JSON.parse(body).message === "Not authenticated") {
+            console.log(JSON.parse(body).message)
+        }
+        else if (!JSON.parse(body).username) {
+            console.log("Please insert a correct username")
+        }
+        else {
+            console.log("username: " + JSON.parse(body).username)
+            console.log("token: " + token.toString())
+        }
     });
 }
