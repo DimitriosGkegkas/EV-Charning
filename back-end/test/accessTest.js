b8023fcf22c99f526db580b57f8fd0ff8f6ec8b4
andrianaavgerinou <andriana.avg@gmail.com>
2021-03-08 14:41:51 +0200

    Add tests Login

--- back-end/test/accessTest.js
+++ back-end/test/accessTest.js
@@ -5,8 +5,119 @@ var expect = chai.expect;
 var urlBase = "https://localhost:8765";
 https = require("https")
 
+describe("Login", function () {
+
+    it("Login Successful", function (done) {
+        const jsonObject =
+        {
+            "username": "admin",
+            "password": "petrol4ever"
+        }
+        request.post({
+            url: "https://localhost:8765/login",
+            rejectUnauthorized: false,
+            json: jsonObject,
+
+        }, function (error, response, body) {
+            expect(body.token).to.exist
+            expect(response).to.have.property("statusCode", 200)
+            expect(error).not.to.exist
+            done(); // callback the test runner to indicate the end...
+        })
+    })
+
+    it("Login Failed: Wrong Password", function (done) {
+        const jsonObject =
+        {
+            "username": "admin",
+            "password": "someRandomPassword"
+        }
+        request.post({
+            url: "https://localhost:8765/login",
+            rejectUnauthorized: false,
+            json: jsonObject,
+
+        }, function (error, response, body) {
+            expect(body.token).not.to.exist
+            expect(response).to.have.property("statusCode", 401)
+            expect(body.message).to.equal("Please Check your password")
+            done(); // callback the test runner to indicate the end...
+        })
+    })
+
+    it("Login Failed: User doesn't exist", function (done) {
+        const jsonObject =
+        {
+            "username": "userDoesNotExistPjhfjksdhfjshdhdgfhksd",
+            "password": "someRandomPassword"
+        }
+        request.post({
+            url: "https://localhost:8765/login",
+            rejectUnauthorized: false,
+            json: jsonObject,
+
+        }, function (error, response, body) {
+            expect(body.token).not.to.exist
+            expect(response).to.have.property("statusCode", 401)
+            expect(body.message).to.equal("Please Check your username")
+            done(); // callback the test runner to indicate the end...
+        })
+    })
+
+    it("Login Failed: no password", function (done) {
+        const jsonObject =
+        {
+            "username": "userDoesNotExistPjhfjksdhfjshdhdgfhksd",
+        }
+        request.post({
+            url: "https://localhost:8765/login",
+            rejectUnauthorized: false,
+            json: jsonObject,
+
+        }, function (error, response, body) {
+            expect(body.token).not.to.exist
+            expect(response).to.have.property("statusCode", 400)
+            expect(body.message).to.equal("Please Provide a Password")
+            done(); // callback the test runner to indicate the end...
+        })
+    })
+
+    it("Login Failed: no username", function (done) {
+        const jsonObject =
+        {
+            "password": "xistPjhfjksdhfjshdhdgfhksd",
+        }
+        request.post({
+            url: "https://localhost:8765/login",
+            rejectUnauthorized: false,
+            json: jsonObject,
+
+        }, function (error, response, body) {
+            expect(body.token).not.to.exist
+            expect(response).to.have.property("statusCode", 400)
+            expect(body.message).to.equal("Please Provide a Username")
+            done(); // callback the test runner to indicate the end...
+        })
+    })
+
+    it("Login Failed: no username or password", function (done) {
+        const jsonObject = {}
+        request.post({
+            url: "https://localhost:8765/login",
+            rejectUnauthorized: false,
+            json: jsonObject,
+
+        }, function (error, response, body) {
+            expect(body.token).not.to.exist
+            expect(response).to.have.property("statusCode", 400)
+            expect(body.message).to.equal("Please Provide a Username")
+            done(); // callback the test runner to indicate the end...
+        })
+    })
+})
+
+
 describe("Logout", function () {
-    // the it function do the test, in this case, the endpoint /cards, that should return 100 cards max
     it("Not Valid Token", function (done) {
 
         request.post({
@@ -92,29 +203,31 @@ describe("Logout", function () {
 
 
     it("Valid Token", function (done) {
-        const jsonObject ={
-        "username": "admin",
-        "password": "petrol4ever"}
+        const jsonObject = {
+            "username": "admin",
+            "password": "petrol4ever"
+        }
 
         request.post({
             url: "https://localhost:8765/login",
             rejectUnauthorized: false
             , json: jsonObject
-            
+
         }, function (error, response, body) {
             expect(body.token).to.exist
 
-            const token= body.token
+            const token = body.token
             request.post({
                 url: "https://localhost:8765/logout",
                 rejectUnauthorized: false,
                 headers: {
-                    "Authorization": 'Bearer ' +token
-                }},function (error, response, body) {
-                    expect(response).to.have.property("statusCode", 200)
- 
-                })
-    
+                    "Authorization": 'Bearer ' + token
+                }
+            }, function (error, response, body) {
+                expect(response).to.have.property("statusCode", 200)
+
+            })
+
 
 
 
