43f89b74823908142e5fa4bcf682c71b23ae112f
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2021-03-08 13:49:58 +0200

    Add Logout Test

--- back-end/test/accessTest.js
+++ back-end/test/accessTest.js
@@ -5,9 +5,9 @@ var expect = chai.expect;
 var urlBase = "https://localhost:8765";
 https = require("https")
 
-describe("Login", function () {
+describe("Logout", function () {
     // the it function do the test, in this case, the endpoint /cards, that should return 100 cards max
-    it("Not Auth", function (done) {
+    it("Not Valid Token", function (done) {
 
         request.post({
             url: "https://localhost:8765/logout",
@@ -24,7 +24,74 @@ describe("Login", function () {
         })
     })
 
-    it("Auth", function (done) {
+    it("Not Token at all", function (done) {
+
+        request.post({
+            url: "https://localhost:8765/logout",
+            rejectUnauthorized: false,
+            headers: {
+                "Authorization": 'Bearer'
+            },
+
+        }, function (error, response, body) {
+
+            expect(response).to.have.property("statusCode", 401)
+            expect(JSON.parse(body).message).to.equal("Not authenticated")
+            done(); // callback the test runner to indicate the end...
+        })
+    })
+
+    it("Not Authorization", function (done) {
+
+        request.post({
+            url: "https://localhost:8765/logout",
+            rejectUnauthorized: false,
+
+
+        }, function (error, response, body) {
+
+            expect(response).to.have.property("statusCode", 401)
+            expect(JSON.parse(body).message).to.equal("Not authenticated")
+            done(); // callback the test runner to indicate the end...
+        })
+    })
+
+    it("Header Format Wrong (token with no bearer)", function (done) {
+
+        request.post({
+            url: "https://localhost:8765/logout",
+            rejectUnauthorized: false,
+            headers: {
+                "Authorization": 'Bfasfsafsafsswafr'
+            },
+
+        }, function (error, response, body) {
+
+            expect(response).to.have.property("statusCode", 401)
+            expect(JSON.parse(body).message).to.equal("Not authenticated")
+            done(); // callback the test runner to indicate the end...
+        })
+    })
+
+    it("Header Format Wrong (Extra word)", function (done) {
+
+        request.post({
+            url: "https://localhost:8765/logout",
+            rejectUnauthorized: false,
+            headers: {
+                "Authorization": 'Bearer someotherWords token'
+            },
+
+        }, function (error, response, body) {
+
+            expect(response).to.have.property("statusCode", 401)
+            expect(JSON.parse(body).message).to.equal("Not authenticated")
+            done(); // callback the test runner to indicate the end...
+        })
+    })
+
+
+    it("Valid Token", function (done) {
         const jsonObject ={
         "username": "admin",
         "password": "petrol4ever"}
