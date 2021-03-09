9a5552c7fbc7cfce346e2c1536fe25633e78b3fb
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2021-03-09 17:12:46 +0200

    Fix front end sessions

--- front-end/controller/dataAccess.js
+++ front-end/controller/dataAccess.js
@@ -1,12 +1,12 @@
 const https = require('https')
 const request =require('request')
-
+const moment =require('moment')
 
 exports.perStation =(req, res, next) => {
 
     const stationID = req.query.ID
-    const periodFrom = req.query.dateFrom
-    const periodTo = req.query.dateTo
+    const periodFrom = moment(Date(req.query.dateFrom)).format("YYYY-MM-DD hh:mm:ss")
+    const periodTo = moment(Date(req.query.dateTo)).format("YYYY-MM-DD hh:mm:ss")
 
     const agentOptions = {
         host: 'localhost'
@@ -21,27 +21,50 @@ exports.perStation =(req, res, next) => {
         , method: 'GET'
         , agent: agent
     }, function (err, resp, body) {
+        if(resp.statusCode!=200){
+
+            res.render( "view-data", { message: "Not Valid Input", body:"",per:""})
+            return
+        }
         res.render( "sessionsPerStation", { "per":"station" ,"body":JSON.parse(body)})
     });
 }
 
 exports.perPoint =(req, res, next) =>{
     const pointID = req.query.ID
-    const periodFrom = req.query.dateFrom
-    const periodTo = req.query.dateTo
+    const periodFrom = moment(new Date(req.query.dateFrom)).format("YYYY-MM-DD HH:mm:ss")
+    const periodTo = moment(new Date(req.query.dateTo)).format("YYYY-MM-DD HH:mm:ss")
 
+    let token
+    try {
+        token = req.cookies.token
+    }
+    catch {
+        console.log("Access Denied")
+        return
+    }
+    const auth = "Bearer " + token;
     const agentOptions = {
         host: 'localhost'
         , port: '8765'
         , path: '/SessionsPerPoint'
+
         , rejectUnauthorized: false
     };
     const agent = new https.Agent(agentOptions);
     request({
         url: "https://localhost:8765/SessionPerPoint/"+pointID+"/"+periodFrom+"/"+periodTo
         , method: 'GET'
+        , headers: {
+            "Authorization": auth
+        }
         , agent: agent
     }, function (err, resp, body) {
+        if(resp.statusCode!=200){
+
+            res.render( "view-data", { message: "Not Valid Input", body:"",per:""})
+            return
+        }
         res.render( "sessionsPerPoint", { "per":"point" ,"body":JSON.parse(body)})
     });
 }
@@ -49,8 +72,8 @@ exports.perPoint =(req, res, next) =>{
 
 exports.perEV =(req, res, next) =>{
     const evID = req.query.ID
-    const periodFrom = req.query.dateFrom
-    const periodTo = req.query.dateTo
+    const periodFrom = moment(Date(req.query.dateFrom)).format("YYYY-MM-DD hh:mm:ss")
+    const periodTo = moment(Date(req.query.dateTo)).format("YYYY-MM-DD hh:mm:ss")
 
     const agentOptions = {
         host: 'localhost'
@@ -64,6 +87,10 @@ exports.perEV =(req, res, next) =>{
         , method: 'GET'
         , agent: agent
     }, function (err, resp, body) {
+        if(resp.statusCode!=200){
+            res.render( "view-data", { message: "Not Valid Input", body:"",per:""})
+            return
+        }
         res.render( "sessionsPerEV", { "per":"ev" ,"body":JSON.parse(body)})
     });
 }
@@ -72,8 +99,8 @@ exports.perEV =(req, res, next) =>{
 
 exports.perProvider=(req, res, next) =>{
     const providerID = req.query.ID
-    const periodFrom = req.query.dateFrom
-    const periodTo = req.query.dateTo
+    const periodFrom = moment(Date(req.query.dateFrom)).format("YYYY-MM-DD hh:mm:ss")
+    const periodTo = moment(Date(req.query.dateTo)).format("YYYY-MM-DD hh:mm:ss")
 
     const agentOptions = {
         host: 'localhost'
@@ -88,13 +115,17 @@ exports.perProvider=(req, res, next) =>{
         , method: 'GET'
         , agent: agent
     }, function (err, resp, body) {
+        if(resp.statusCode!=200){
+            res.render( "view-data", { message: "Not Valid Input", body:"",per:""})
+            return
+        }
         res.render( "sessionsPerProvider", { "per":"provider" ,"body":JSON.parse(body)})
     });
 
 }
 
 exports.viewData= (req, res, next)=>{
-    res.render("view-data",{body:{}})
+    res.render("view-data",{body:{},message:""})
 }
 
 
