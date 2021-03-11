ead4fa4ccd37f01ce182804efeacff09ab9d931e
AnnaXar <annaxar3@gmail.com>
2021-03-11 20:41:31 +0200

    Add api key to logoutTest (back-end)

--- cli-client/test/sessionsperstation.js
+++ cli-client/test/sessionsperstation.js
@@ -38,7 +38,7 @@ describe("SessionsPerStation", function () {
         sinon.stub(console, "log").callsFake(mockedLog)
         login("admin","petrol4ever")
         setTimeout(()=>{
-            apikey = consoleOutput[1].split(' ')[2];
+            apikey = consoleOutput[1].split(' ')[2]
             console.log.restore()
             consoleOutput = []
             done() },1000)
@@ -47,16 +47,7 @@ describe("SessionsPerStation", function () {
 
 
 
-    it('no station id ',function (done) {
-        this.timeout(5000)
-
-        SessionsPerStation = (undefined,"2019-08-01 20:14:17","2019-08-01 22:01:04",apikey)
-        setTimeout(()=>{
-            expect(consoleOutput).to.be.deep.equal(["Please Provide a station Id"])
-            done()
-        },1000)
-          
-    });
+  
     it('wrong station id ',function (done) {
         this.timeout(5000)
 
@@ -69,17 +60,7 @@ describe("SessionsPerStation", function () {
     });
     
 
-    it('no date from  ',function (done) {
-       
-        this.timeout(5000)
-
-        SessionsPerStation = ("CA-311",undefined,"2019-08-01 22:01:04",apikey)
-        setTimeout(()=>{
-            expect(consoleOutput).to.be.deep.equal(["Please Provide a Date From"])
-            done()
-        },1000)
-          
-    })
+ 
 
     it('wrong date from  ',function (done) {
        
@@ -93,17 +74,7 @@ describe("SessionsPerStation", function () {
           
     })
 
-it('no date to ',function (done) {
-       
-    this.timeout(5000)
 
-    SessionsPerStation = ("CA-311","2019-08-01 20:14:17",undefined,apikey)
-    setTimeout(()=>{
-        expect(consoleOutput).to.be.deep.equal(["Please Provide a Date To"])
-        done()
-    },1000)
-      
-}) 
 it('wrong date to ',function (done) {
        
     this.timeout(5000)
@@ -115,17 +86,7 @@ it('wrong date to ',function (done) {
     },1000)
       
 })
-it('no apikey ',function (done) {
-       
-    this.timeout(5000)
 
-    SessionsPerStation = ("CA-311","2019-08-01 20:14:17","2019-08-01 22:01:04",undefined)
-    setTimeout(()=>{
-        expect(consoleOutput).to.be.deep.equal(["Not authenticated"])
-        done()
-    },1000)
-      
-}) 
 it('wrong apikey ',function (done) {
        
     this.timeout(5000)
